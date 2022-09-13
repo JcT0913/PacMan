7569222f7df3dd627f32eb35d2d792cb15e64f56
Jiachen Tang <tjc9606ok@gmail.com>
2022-09-13 02:54:54 -0400

    test enemy's NavMeshAgent

--- Assignment 1 Pacman/Assets/EnemyController.cs
+++ Assignment 1 Pacman/Assets/EnemyController.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
+    public Transform playerPosition;
+
+    private NavMeshAgent enemyAgent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Ray ray = new Ray(transform.position, playerPosition.position - transform.position);
+        Debug.DrawRay(transform.position, playerPosition.position - transform.position, Color.blue);
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit);
+        if (hit.collider.tag == "Player")
+        {
+            enemyAgent.SetDestination(hit.transform.position);
+        }
     }
 }
