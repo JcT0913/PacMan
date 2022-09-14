a065b7bf2d84041ef99f7748c224d2b51822ba4a
Jiachen Tang <tjc9606ok@gmail.com>
2022-09-13 23:29:59 -0400

    add points & highest points to UI

---
+++ Assignment 1 Pacman/Assets/TextController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TextController : MonoBehaviour
+{
+    public TextMeshProUGUI pointsText;
+    public TextMeshProUGUI highestPointsText;
+
+    private int points = 0;
+    private int highestPoints = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointsText.text = points.ToString() + " Points";
+        highestPointsText.text = "Highest Record: " + highestPoints.ToString() + " Points";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
